be9348d2e080e386775c91fab40f34285fc22359
alifanovelia05 <69952596+Alifanoveliasukma@users.noreply.github.com>
2023-05-11 21:46:50 +0700

    sementara

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl bg-slate-600">
-      Hello world!
-    </h1>
+import React from 'react';
+
+//import routes and route
+import {Routes, Route} from 'react-router-dom'
+
+//import components
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+//import pages
+import Home from './pages/Home'
+import PropertyDetails from './pages/PropertyDetails';
+
+const App = () => {
+  return ( 
+  <div>
+    <Header />
+    <Footer />
+  </div>
   )
 }
+
+export default App;
