b18f39823bcbe51745b3af7068475eb04cc4f62a
alifanovelia05 <69952596+Alifanoveliasukma@users.noreply.github.com>
2023-05-11 20:43:33 +0700

    basic design

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl bg-slate-600">
-      Hello world!
-    </h1>
-  )
+import React from 'react';
+
+//import routes and route
+import {Routes, Route} from 'react-router-dom'
+
+//import components
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+//import pages
+import Home from './pages/Home'
+import PropertyDetails from './pages/PropertyDetails';
+
+const App = () => {
+  return <div className='max-w-[1440px] mx-auto bg-white'>
+    <Header />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/' element={<PropertyDetails />} />
+    </Routes>
+    <Footer />
+  </div>;
 }
+
+export default App;
